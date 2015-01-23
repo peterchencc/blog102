6197669d69d195065fd892f1f2d9a75a2896adaa
peter_summers <peter@summers.com.tw>
2015-01-23 17:59:27 +0800

    hide posts search

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,5 @@
 class PostsController < ApplicationController
   def index
-        if params[:keyword]
-      @posts = Post.where( [ "name like ?", "%#{params[:keyword]}%" ]).page(params[:page]).per(5)
-    else
-      @posts = Post.page(params[:page]).per(5)
-    end
+    @posts = Post.page(params[:page]).per(5)
   end
 end
--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,10 +1,11 @@
 class StaticsController < ApplicationController
   def index
-    @posts = Post.all
-    if params[:keyword]
-      @posts = Post.where( [ "name like ?", "%#{params[:keyword]}%" ]).page(params[:page]).per(5)
-    else
-      @posts = Post.page(params[:page]).per(5)
-    end
+    @posts = Post.page(params[:page]).per(5)
+    
+    # if params[:keyword]
+    #   @posts = Post.where( [ "name like ?", "%#{params[:keyword]}%" ]).page(params[:page]).per(5)
+    # else
+    #   @posts = Post.page(params[:page]).per(5)
+    # end
   end
 end
