6bb16bbeee690649745ed36816503c2997fb650f
peter_summers <peter@summers.com.tw>
2015-01-26 19:04:04 +0800

    後台css 調整

--- app/models/admin/post.rb
+++ app/models/admin/post.rb
@@ -1,3 +1,3 @@
 class Admin::Post < Post
-  default_scope { order('ranking, created_at DESC') }
+  # default_scope { order('created_at DESC') }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   require_dependency 'admin/post'
   mount_uploader :avatar, AvatarUploader
+  default_scope { order('ranking ASC') }
 end
